539c3de72d3679edb0120d450f72719cc013e46a
joshua360x <jkw5499@psu.edu>
2021-12-06 20:50:40 -0500

    fixed erros to allow test to run and pass

--- test/example.test.js
+++ test/example.test.js
@@ -6,7 +6,7 @@ const test = QUnit.test;
 test('time to test a function', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    let game = {}
+    let game = {};
     const expected = '<div class="game"><div><p></p><p></p></div><div><p></p><p></p></div></div>';
     
     //Act 
@@ -14,8 +14,8 @@ test('time to test a function', (expect) => {
     const actual = renderGame(game).outerHTML;
 
 
-    const expected1 = true
-    const actual1 = renderGame(game).outerHTML.includes('div')
+    const expected1 = true;
+    const actual1 = renderTeam(game).outerHTML.includes('div');
 
     //Expect
     // Make assertions about what is expected versus the actual result
